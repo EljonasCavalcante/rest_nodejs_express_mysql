3041ab421b82ef7ab0e3badfa0c00ae05bf58dbc
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-02-17 15:37:08 -0300

    feat:creating backend configuration for pick up giving and sending data to server

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const consign = require('consign')
-
+const bodyParser = require('body-parser')
 
 // essa função configura o app e devolve o app
 module.exports = () => {
     const app = express()
 
+    app.use(bodyParser.urlencoded({extended : true}))
+    app.use(bodyParser.json())
     consign()
         .include('controllers')
         .into(app)
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,4 +3,8 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando GET'))
 
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está de rota de atendimento e está realizando um POST')
+    })
 }
