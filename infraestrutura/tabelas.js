a311258d3b6ad4b5c31faa64e8621d9d15f77de9
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-02-19 23:53:58 -0300

    feat:add tabelas

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -2,11 +2,10 @@
      init(conexao){
         this.conexao = conexao
         this.criarAtendimentos()
-     }
-     
-     // "CREATE TABLE IF NOT EXISTS - importante colocar esse comando se dará erro"
+     }       
+
      criarAtendimentos() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimnento (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status  varchar(20) NOT NULL, observacao text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimnentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
 
         this.conexao.query(sql, erro => {
              if(erro) {
