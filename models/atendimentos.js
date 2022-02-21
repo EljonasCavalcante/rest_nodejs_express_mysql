0ae3a093d45a332bf89083a8f9a104cbe79bc10b
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-02-21 16:14:53 -0300

    feat: error handling whit msg

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -4,9 +4,33 @@ const conexao = require('../infraestrutura/conexao')
 class Atendimento {
     adiciona(atendimento, res) {
 
-        const dataCriacao = moment().format('YYYY-MM-DD HH-MM-SS')
-        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH-MM-SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valida: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome:'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
         const sql = 'INSERT INTO atendimentos SET ?'
 
 
@@ -18,6 +42,9 @@ class Atendimento {
                 res.status(201).json(resultado)
             }
         })
+
+        }
+        
     }
 }
 
