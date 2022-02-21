6536e425618f04b0572ce0bea87777e422548fef
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-02-21 15:37:48 -0300

    feat: config Msg ERRO 400 or 201

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,7 +2,7 @@ const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
 
         const dataCriacao = moment().format('YYYY-MM-DD HH-MM-SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH-MM-SS')
@@ -12,10 +12,10 @@ class Atendimento {
 
         conexao.query(sql, atendimentoDatado, (erro, resultado) => {
             if(erro) {
-                console.log(erro)
+                res.status(400).json(erro)
 
             } else {
-                console.log(resultado)
+                res.status(201).json(resultado)
             }
         })
     }
